1cef31252e1a402d05e808272f7d0a935463e034
Deven Kalra <deven@Mac.local>
2022-04-29 03:25:04 +0530

    First

---
+++ src/main/java/com/cleo/NFT_Banking/NftBankingApplication.java
@@ -0,0 +1,13 @@
+package com.cleo.NFT_Banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleo/NFT_Banking/NftBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleo.NFT_Banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
